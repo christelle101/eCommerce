a3b3e9ef3e34baaefa0d3aa251780812d4fbd8b1
christelle101 <nomentsoa.randriamahefa@etu.univ-savoie.fr>
2021-10-01 15:36:01 +0200

    Version 2

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const express = require('express');
 //create app
 const app = express();
 
+//Send back a raw message every time the server got an 
+//incoming request
+app.get('*', (req, res) => {
+
+    res.send('Express response');
+
+});
+
 //listen to the 3000 port
 app.listen(3000);
 
 //print out server location
-console.log("Server successfully running of port: 3000");
+console.log("Server successfully running on port: 3000");
