206f64f225e37e2d721c936be91200ba2bba986f
christelle101 <christelle.randriamahefa@gmail.com>
2021-11-29 21:55:28 +0100

    Frontend Update

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -20,30 +20,4 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
 })
 export class Tab1PageModule {}
 
-@Component({
-  selector: 'slides-example',
-  template: `
-    <ion-content>
-      <ion-slides pager="true" [options]="slideOpts">
-        <ion-slide>
-          <h1>Slide 1</h1>
-        </ion-slide>
-        <ion-slide>
-          <h1>Slide 2</h1>
-        </ion-slide>
-        <ion-slide>
-          <h1>Slide 3</h1>
-        </ion-slide>
-      </ion-slides>
-    </ion-content>
-  `
-})
-export class SlideExample {
-  // Optional parameters to pass to the swiper instance.
-  // See http://idangero.us/swiper/api/ for valid options.
-  slideOpts = {
-    initialSlide: 1,
-    speed: 400
-  };
-  constructor() {}
-}
+
